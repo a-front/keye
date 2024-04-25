18474d794805a15e97db3a61400fd21ae4bc94fa
zzb <842275151@qq.com>
2024-04-25 14:24:51 +0800

    init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '东边吹来一阵风';
+export const SITE_DESCRIPTION = '';
